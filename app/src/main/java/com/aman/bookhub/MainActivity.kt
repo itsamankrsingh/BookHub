c229bfca0ff664f659dcdba8fb30c96197d500ac
itsamankrsingh <itsamankrsingh@gmail.com>
2020-05-17 10:11:25 +0530

    Second Commit

--- app/src/main/java/com/aman/bookhub/MainActivity.kt
+++ app/src/main/java/com/aman/bookhub/MainActivity.kt
@@ -18,63 +18,112 @@ class MainActivity : AppCompatActivity() {
     lateinit var toolbar: androidx.appcompat.widget.Toolbar
     lateinit var frameLayout: FrameLayout
     lateinit var navigationView: NavigationView
+    var previousMenuItem: MenuItem?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawerLayout=findViewById(R.id.drawerLayout)
-        coordinatorLayout=findViewById(R.id.coordinatorLayout)
-        toolbar=findViewById(R.id.toolbar)
-        frameLayout=findViewById(R.id.frame)
-        navigationView=findViewById(R.id.navigationView)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout = findViewById(R.id.frame)
+        navigationView = findViewById(R.id.navigationView)
 
         setUpToolbar()
 
-        val actionBarDrawerToggle=ActionBarDrawerToggle(this@MainActivity,drawerLayout,
-            R.string.open_drawer,R.string.close_drawer)
+        openDashboard()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@MainActivity, drawerLayout,
+            R.string.open_drawer, R.string.close_drawer
+        )
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
         navigationView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.dashboard->{
-                    Toast.makeText(this@MainActivity,"Clicked on DashBoard",Toast.LENGTH_SHORT).show()
+
+            if(previousMenuItem!=null){
+                previousMenuItem?.isChecked=false
+            }
+            it.isChecked=true
+            it.isCheckable=true
+            previousMenuItem=it
+
+            when (it.itemId) {
+                R.id.dashboard -> {
+                   openDashboard()
+                    drawerLayout.closeDrawers()
                 }
 
-                R.id.profile->{
-                    Toast.makeText(this@MainActivity,"Clicked on Profile",Toast.LENGTH_SHORT).show()
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, ProfileFragment())
+                        .commit()
+
+                    supportActionBar?.title = "Profile"
+                    drawerLayout.closeDrawers()
 
                 }
 
-                R.id.aboutApp->{
-                    Toast.makeText(this@MainActivity,"Clicked on About App",Toast.LENGTH_SHORT).show()
+                R.id.aboutApp -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, AboutAppFragment())
+                        .commit()
+
+                    supportActionBar?.title = "About App"
+                    drawerLayout.closeDrawers()
                 }
 
-                R.id.favourites->{
-                    Toast.makeText(this@MainActivity,"Clicked on Favourites",Toast.LENGTH_SHORT).show()
+                R.id.favourites -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame, FavouritesFragment())
+                        .commit()
+
+
+                    supportActionBar?.title = "Favourites"
+                    drawerLayout.closeDrawers()
                 }
             }
             return@setNavigationItemSelectedListener true
         }
     }
 
-    fun setUpToolbar(){
+    fun setUpToolbar() {
         setSupportActionBar(toolbar)
-        supportActionBar?.title="Toolbar Title"
+        supportActionBar?.title = "Toolbar Title"
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        val id=item.itemId
+        val id = item.itemId
 
-        if (id==android.R.id.home){
+        if (id == android.R.id.home) {
             drawerLayout.openDrawer(GravityCompat.START)
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun openDashboard() {
+        val fragment = DashboardFragment()
+        val transcation = supportFragmentManager.beginTransaction()
+        transcation.replace(R.id.frame, fragment)
+        transcation.commit()
+
+        supportActionBar?.title = "Dashboard"
+    }
+
+    override fun onBackPressed() {
+        val frag=supportFragmentManager.findFragmentById(R.id.frame)
+
+        when (frag){
+            !is DashboardFragment -> openDashboard()
+
+            else->super.onBackPressed()
+        }
     }
 }
